2e153a904827927de5ebf6ebc0cfba360951d32c
Emhope <michailopalev02@gmail.com>
2023-11-24 14:09:10 +0300

    init commit

---
+++ main.py
@@ -0,0 +1,77 @@
+import pygame
+import numpy as np
+import scene_tools
+import itertools
+import time
+import random
+
+
+def chase(chaser: scene_tools.Object, goal: scene_tools.Object, t=1.0, max_speed=10.0, min_distance=20) -> None:
+    if np.linalg.norm(goal.get_middle_point() - chaser.get_middle_point()) < min_distance:
+        chaser.set_speed([0, 0])
+        return True
+
+    goal_future_pos = goal.get_middle_point() + goal.get_speed() * t
+    speed = goal_future_pos - chaser.get_middle_point()
+    
+    if np.linalg.norm(speed) > max_speed:
+        speed *= (max_speed / np.linalg.norm(speed))
+    
+    chaser.set_speed(speed)
+    return False
+
+
+
+def main(width=600, height=400, fps=30):
+    '''
+    robots are red
+    not passed goals are blue
+    passed goals are green
+    '''
+    pygame.init()
+    clock = pygame.time.Clock()
+    screen = pygame.display.set_mode((width, height))
+
+    max_speed = 100
+    robots = [scene_tools.Object(scene_tools.create_square([random.randint(0, width), random.randint(0, height)], 10), color=(255, 0, 0)) for _ in range(random.randint(5, 7))]
+    list_goals = [scene_tools.Object(scene_tools.create_square([random.randint(50, width), random.randint(50, height)], 10), color=(30, 30, 230)) for _ in range(random.randint(13, 15))]
+    goals = iter(list_goals)
+    current_goal = next(goals)
+
+    start = time.perf_counter()
+    running = True
+    while running:
+        clock.tick(fps) #similar to timerDelay
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill((100, 100, 100))
+
+        if chase(chaser=robots[-1], goal=current_goal, max_speed=max_speed):
+            try:
+                current_goal.set_color((0, 255, 0))
+                current_goal = next(goals)
+            except:
+                running = False
+
+        for i, robot in enumerate(robots):
+            if i < len(robots) - 1:
+                chase(chaser=robot, goal=robots[i+1], max_speed=max_speed, min_distance=60)    
+            robot.simulate(1/fps)
+            robot.draw(screen)
+
+        for goal in list_goals:
+            goal.draw(screen)
+
+        pygame.display.flip()
+
+
+    pygame.quit()
+    print(f'time: {round(time.perf_counter() - start, 2)}s')
+    
+
+
+if __name__ == '__main__':
+    main(fps=30, width=1200, height=720)
---
+++ scene_tools.py
@@ -0,0 +1,64 @@
+import pygame
+import numpy as np
+
+def create_square(pos, r):
+    return np.array([pos + r * np.array([np.cos(angle), np.sin(angle)]) for angle in map(np.deg2rad, [45, 135, 225, 315])], dtype=int)
+
+
+def rotate(arr: np.ndarray, alpha: float) -> np.array:
+    '''
+    rotating array by alpha degs
+    '''
+    alpha = np.deg2rad(alpha)
+    rot = np.array([
+        [np.cos(alpha) ,-np.sin(alpha)],
+        [np.sin(alpha), np.cos(alpha)]
+    ])
+
+    return arr.dot(rot)
+
+
+def vector_angle(vector: np.ndarray) -> float:
+    angle = np.rad2deg(np.arctan(vector[1] / vector[0]))
+    if vector[0] < 0 and vector[1] < 0:
+        return angle + 180
+    elif vector[0] < 0:
+        return 90 - angle
+    elif vector[1] < 0:
+        return 270 - angle
+    return angle
+
+class Object:
+    def __init__(self, points, width=1, color=(0, 255, 0)):
+        self.points = points.astype(np.float64)
+        self.width = width
+        self.color = color
+        self.speed = np.array([0.0, 0.0])
+
+
+    def simulate(self, dt):
+        self.points += self.speed * dt
+    
+    def get_middle_point(self):
+        return self.points.mean(axis=0)
+    
+    def get_speed(self):
+        return self.speed
+    
+    def rotate(self, angle):
+        self.points = rotate(self.points, angle)
+
+    def set_color(self, color):
+        self.color = color
+
+    def set_speed(self, new_speed):
+        if type(new_speed) != np.ndarray:
+            self.speed = np.array(new_speed)
+        else:
+            self.speed = np.array(new_speed)
+    
+    def draw(self, screen):
+        pygame.draw.polygon(surface=screen, color=self.color, points=self.points, width=self.width)
+
+
+        
